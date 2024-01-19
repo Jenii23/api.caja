4d4808451477838e2477a524e8683a8e9fe0f025
Jenii23 <jenifferal23972@hotmail.com>
2024-01-19 16:46:05 -0500

    desarrollo de la segunda semana.

---
+++ src/main/java/ec/edu/espam/api/caja/exceptions/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package ec.edu.espam.api.caja.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/ec/edu/espam/api/caja/repository/MovimientoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.espam.api.caja.repository;
-
-import ec.edu.espam.api.caja.domain.Movimiento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovimientoRepositorio extends JpaRepository<Movimiento, Long> {
-}
